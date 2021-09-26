2277c5d2cae9b138bbb9a435753cab47e1101b7d
EdgarLunaG-442 <ef.luna442@uniandes.edu.co>
2021-09-25 20:12:27 -0500

    Se modifica postgres por postgresql

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 
 def create_app(config_name):
     app = Flask(__name__)  
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace('postgres','postgresql')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY']='frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
